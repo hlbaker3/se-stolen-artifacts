6dacab02777b804c2a5a63448543fe62b133165b
hlbaker3 <hallielbaker@gmail.com>
2026-05-03 16:27:50 +0100

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,20 @@
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 import { NextResponse } from 'next/server';
 
-export function middleware() {
+const isProtectedRoute = createRouteMatcher(['/upload(.*)']);
+
+export default clerkMiddleware(async (auth, req) => {
+  if (isProtectedRoute(req)) {
+    const { userId } = await auth();
+    if (!userId) {
+      const signInUrl = new URL('/signin', req.url);
+      signInUrl.searchParams.set('redirect_url', req.nextUrl.pathname);
+      return NextResponse.redirect(signInUrl.toString());
+    }
+  }
   return NextResponse.next();
-}
+});
 
 export const config = {
-  matcher: ['/((?!_next|favicon.ico).*)'],
+  matcher: ['/((?!_next|favicon.ico|.*\\..*).*)'],
 };
